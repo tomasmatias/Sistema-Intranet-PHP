5a4fb316142adcb62b86ff3b8e9fff8aba75b375
Tomas Matias <38861129+tomasmatias@users.noreply.github.com>
2022-01-10 16:34:32 -0300

    Update registrar.php

--- admin/registrar.php
+++ admin/registrar.php
@@ -5,7 +5,7 @@
     $sv="localhost";
     
     $obj_conexion = 
-    mysqli_connect($sv,$user,$password,$sv);
+    mysqli_connect($sv,$user,$password,$bd);
     if(!$obj_conexion)
     {
         echo "<h3>No se ha podido conectar PHP - MySQL, verifique sus datos.</h3><hr><br>";
